ce06f67130234b170c914a951fcf84f7ddc69920
corderodedios182 <corderodedios182@gmail.com>
2019-08-03 16:58:37 -0500

    Numeros Primos

--- Ejercicios_programacion.R
+++ Ejercicios_programacion.R
@@ -31,7 +31,31 @@ for(i in 2:40){
 tabla <- data.frame(x = x,y = x %% 2)
 sum(tabla[which(tabla$x < 4000000 & tabla$y ==0),])
 
-#
+#Los factores primos de 13195 son 5, 7, 13 y 29.
+
+#¿Cuál es el factor primo más grande del número 600851475143?
+
+#Funcion que encuentra numero es primo
+es_primo <- function(num){
+  
+  contador = 0
+  #Cuantos numeros son divisores tal que el residuo es 0
+  for(i in 1:num){
+    if(num %% i == 0){
+      contador = contador + 1}}
+  #Si solo tuvo 2 divisores 1 y el mismo numero, es primo
+  if(contador == 2){
+    print("Es primo")}
+  else{print("No es primo")}
+  }
+  
+es_primo(100)
+
+10 %% 5
+
+x = c(2,3,5,7,11,13,17,19,23,24,31,37,41,43)
+
+
 
 
 
