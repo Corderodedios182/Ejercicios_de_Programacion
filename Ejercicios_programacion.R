a35da6c75de9d0afb8daf7afe3fec104a7df3977
corderodedios182 <corderodedios182@gmail.com>
2019-08-06 22:56:39 -0500

    No grafica

--- Ejercicios_programacion.R
+++ Ejercicios_programacion.R
@@ -128,25 +128,3 @@ tabla[which(tabla$x == 0),]
 
 #Por lo tanto: el maximo factor primo del numero 600851475143 es 6857 :)
 6857*1471*839*71 == 600851475143
-
-library(ggplot2)
-theme_set(theme_classic())
-
-# Histogram on a Continuous (Numeric) Variable
-g <- ggplot(mpg, aes(displ)) + scale_fill_brewer(palette = "Spectral")
-
-g + geom_histogram(aes(fill=class), 
-                   binwidth = .1, 
-                   col="black", 
-                   size=.1) +  # change binwidth
-  labs(title="Histogram with Auto Binning", 
-       subtitle="Engine Displacement across Vehicle Classes")  
-
-g + geom_histogram(aes(fill=class), 
-                   bins=5, 
-                   col="black", 
-                   size=.1) +   # change number of bins
-  labs(title="Histogram with Fixed Bins", 
-       subtitle="Engine Displacement across Vehicle Classes") 
-
-
